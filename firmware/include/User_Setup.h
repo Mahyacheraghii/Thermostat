29c5cb353a59af75c9c61fbc63486ffc4a21ef21
Mahya Cheraghi <mahya_ch@yahoo.com>
2026-01-03 12:20:56 +0330

    fix: lcd pins

---
+++ firmware/include/User_Setup.h
@@ -0,0 +1,11 @@
+#define ILI9341_DRIVER
+#define TFT_MOSI 23
+#define TFT_MISO 19
+#define TFT_SCLK 18
+#define TFT_CS 15
+#define TFT_DC 2
+#define TFT_RST 4
+#define TOUCH_CS 5
+#define SPI_FREQUENCY 20000000
+#define SPI_READ_FREQUENCY 16000000
+#define SPI_TOUCH_FREQUENCY 2500000
